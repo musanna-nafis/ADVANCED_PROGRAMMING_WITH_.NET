99a29392572d6029d6c7fb850926f51c3b8b6860
musanna-nafis <hasanalmusanna7@gmail.com>
2022-05-24 18:58:14 +0600

    Submit assignment1 student_cv

---
+++ Assignment1/Assignment1/Controllers/CVController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Referance()
+        {
+            return View();
+        }
+    }
+}
